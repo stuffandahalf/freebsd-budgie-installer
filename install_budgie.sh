21b046a2ba4359834f2d00f5dbc0865e0a492b32
Gregory Norton <gregory.norton@me.com>
2019-07-14 04:14:01 -0400

    Added initial script

---
+++ install_budgie.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+
+set -e
+
+git clone https://github.com/solus-project/budgie-desktop
+cd budgie-desktop
+git checkout 10.5
+git submodule update --init
+
+sudo pkg install \
+	accountsservice \
+	alsa-lib \
+	glib \
+	gnome-desktop \
+	gnome-settings-daemon \
+	gobject-introspection \
+	gtk3 \
+	ibus \
+	libgnome \
+	libnotify \
+	libpeas \
+	pulseaudio \
+	libwnck3 \
+	meson \
+	mutter \
+	polkit \
+	upower \
+	pkgconf \
+	vala \
+	gtk-doc \
+	sassc \
+	cmake \
+	e2fsprogs-libuuid \
+	gettext \
+	gnome-menus
+
+meson build --prefix=/usr --sysconfdir=/etc -Dwith-bluetooth=false
+ninja -j$(($(getconf _NPROCESSORS_ONLN)+1)) -C build
